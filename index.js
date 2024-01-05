91d9ac445e389abac53fb8de7fc00f954c13aeb3
DianaTuei <memotuei@gmail.com>
2024-01-05 17:27:54 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+const main = document.getElementById('main');
+main.remove();
+
+const body = document.querySelector('body');
+body.innerHTML = '<h1 id="victory"></h1>'
+
+const newHeader = document.querySelector('h1');
+newHeader.textContent = "Diana is the champion";
+
+
+// const newHeader = document.createElement('h1');
+// document.body.append(newHeader);
+// newHeader.id = 'victory';
+// newHeader.textContent = 'Diana is the champion';
